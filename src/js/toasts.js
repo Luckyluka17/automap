98217471ce0f031f3c14c1ec59e78cbd2b986d5b
Luckyluka17 <63603989+Luckyluka17@users.noreply.github.com>
2022-10-15 15:23:49 +0200

    Réorganisation du github

--- src/js/toasts.js
+++
@@ -1,9 +0,0 @@
-toast1()
-
-function toast1() {
-    var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-    var toastList = toastElList.map(function(toastEl) {
-        return new bootstrap.Toast(toastEl)
-    })
-    toastList.forEach(toast => toast.show())
-  }
